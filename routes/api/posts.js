4a37faf11dc5dcc42c19104d00780278d54314cb
gan022 <gan022@my.utsa.edu>
2021-01-29 13:41:39 -0600

    added likes and comment, create/delete

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -91,8 +91,119 @@ router.delete('/:id', auth, async (req, res) => {
             return res.status(404).json({msg: "Post not found"})
         }
 
-        res.status(500).send('Server Error: get all posts');
+        res.status(500).send('Server Error: Delete post');
     }
 });
+
+// @route   PUT api/like/:id
+// @desc    Like a post
+// @access  Private
+router.put('/like/:id', auth, async (req, res) => {
+    try {
+        const post =  await Post.findById(req.params.id);
+
+        // Check if user liked the post already
+        if(post.likes.filter(like=> like.user.toString() === req.user.id).length > 0){
+            return res.status(400).json({ msg: 'Post already liked'});
+        }
+
+        post.likes.unshift({ user: req.user.id});
+
+        await post.save();
+        res.json(post.likes);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error: Like a post');
+    }
+});
+
+// @route   PUT api/like/:id
+// @desc    Unlike a post
+// @access  Private
+router.put('/unlike/:id', auth, async (req, res) => {
+    try {
+        const post =  await Post.findById(req.params.id);
+
+        // Check if user liked the post
+        if(post.likes.filter(like=> like.user.toString() === req.user.id).length === 0){
+            return res.status(400).json({ msg: 'Post wasn\'t liked yet'});
+        }
+
+        // Get index to remove
+        const removeIndex = post.likes.map(like => like.user.toString()).
+            indexOf(req.user.id);
+        post.likes.splice(removeIndex, 1);
+
+        await post.save();
+        res.json(post.likes);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error: Like a post');
+    }
+});
+
+// @route   POST api/comment/:id
+// @desc    Comment on a post
+// @access  Private
+router.post('/comment/:id', [auth, [check('text', 'Text is required').not().isEmpty()]],
+    async(req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        try {
+            const user = await User.findById(req.user.id).select('-password');
+            const post = await Post.findById(req.params.id);
+
+            const newComment = new Post ({
+                text: req.body.text,
+                name: user.name,
+                avatar: user.avatar,
+                user: req.user.id
+            });
+
+            post.comments.unshift(newComment);
+            await post.save();
+
+            res.json(post.comments);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error: create comment');
+        }
+});
+
+// @route   Delete api/comment/:id/:comment_id
+// @desc    Delete comment on a post
+// @access  Private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) =>{
+    try {
+        const post =  await Post.findById(req.params.id);
+
+        // Pull out comment
+        const comment = post.comments.find(comment => comment.id === req.params.comment_id);
+        // check if comment exists
+        if (!comment){
+            return res.status(404).json({msg: 'Comment does not exist' });
+        }
+        // check if user owns comment to be deleted
+        if (comment.user.toString() !== req.user.id){
+            return res.status(401).json({msg: 'User is not authorized'});
+        }
+
+        // Get index to remove
+        const removeIndex = post.comments.map(comment => comment.user.toString()).
+        indexOf(req.user.id);
+        post.comments.splice(removeIndex, 1);
+
+        await post.save();
+        res.json(post.comments);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error: delete comment');
+    }
+});
+
+//todo -- include an update comment, might have to teach myself
 
 module.exports = router;
