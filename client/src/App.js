504f563e600fcc901cbef0d1a30326513b486a42
max-elixir <gan022@my.utsa.edu>
2021-03-24 16:22:38 -0500

    alert system added, registering includes alerts

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/layouts/Navbar';
 import Landing from './components/layouts/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alert from './components/layouts/Alert';
 //Redux
 import {Provider} from 'react-redux';
 import store from './store';
@@ -15,6 +16,7 @@ const App = () => (
         <Navbar />
         <Route exact path='/' component={Landing}/>
         <section className="container">
+            <Alert />
             <Switch>
                 <Route exact path="/register" component={Register}/>
                 <Route exact path="/login" component={Login}/>
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,20 +1,28 @@
 import React, { Fragment, useState } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { setAlert } from '../../action/alert';
+import PropTypes from 'prop-types';
 //import axios from 'axios';
 
-const Register = () => {
+const Register = ({setAlert}) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
         password: '',
         password2: ''
     });
+
     const {name, email, password, password2} = formData;
-    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const onChange = e =>
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+
     const onSubmit = async e => {
         e.preventDefault();
         if (password !== password2) {
-            console.log('Passwords do not match');
+            setAlert('Passwords do not match', 'danger');
+            //console.log('Passwords do not match');
         } else {
             console.log('SUCCESS');
             /*const newUser = {
@@ -87,4 +95,11 @@ const Register = () => {
     );
 };
 
-export default Register;
+Register.propTypes = {
+    setAlert: PropTypes.func.isRequired
+};
+
+export default connect(
+    null,
+    { setAlert }
+    )(Register);
